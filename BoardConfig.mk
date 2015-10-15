54d12d6344f2cb0e0c28601ac6d02b4167a03ba3
Chet Kener <Cl3Kener@gmail.com>
2015-10-15 14:58:01 -0400

    Do Not Odex User builds
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ TARGET_USES_ION := true
 ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),user)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT := false
     endif
   endif
 endif
